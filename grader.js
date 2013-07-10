175cb449989ec32f307d9b81fb6936d17870c91d
Matthew Campbell <whariki@gmail.com>
2013-07-10 23:45:15 +1000

    Tidied up functions - pretesting.

--- grader.js
+++ grader.js
@@ -42,8 +42,8 @@ var DEBUG_LEVEL = 0;   // 0 = no debug
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-	console.log("%s does not exist. Exiting.", instr);
-	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -52,72 +52,63 @@ var assertUrlForm = function(inUrl) {
     var instr = inUrl.toString();
     if (DEBUG_LEVEL == 1) { console.error(instr); }
     if (inUrl.slice(0,4)!="http") {
-	console.log("%s must start with http.", instr);
-	process.exit(1);
+        console.log("%s must start with http.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
-
 var cheerioHtmlStr = function(htmlStr) {
-    if (DEBUG_LEVEL == 1) { console.error(htmlStr); }
+    // uses cheerio to parse a string containing an html document.
     return cheerio.load(htmlStr);
 };
 
 var loadChecks = function(checksfile) {
+    // loads a list of tags that need to be checked for
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    if (DEBUG_LEVEL == 3) { console.error($); }
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-	var present = $(checks[ii]).length > 0;
-	out[checks[ii]] = present;
-    }	
-    return out;
-};
-
-var checkUrl = function(result, response) {
-    if (result instanceof Error) {
-	console.error('Error: ' + util.format(result.message));
-	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    } else {
-	if (DEBUG_LEVEL == 2) { console.error(result.toString()); }
-	return result.toString();
-    }
-};
-
 var buildfn = function(checksfile) {
+    // creates the function to use as the 'complete' action when reading
+    // from the url.
+    //
+    // the function created is parameterised by "checksfile", the path
+    // to the file to check.
     var response2console = function(result, response) {
-	if (result instanceof Error) {
-	    console.error('Error: ' + util.format(result.message));
-	} else {
-	    var checkJson = checkHtmlUrl(result, checksfile);
-	    var outJson = JSON.stringify(checkJson, null, 4);
-	    console.log(outJson);
-	}
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(result.message));
+        } else {
+            var checkJson = checkHtml(result, checksfile, INPUT_STRING);
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);
+        }
     };
     return response2console;
 };
 
-
-var checkHtmlUrl = function(inStr, checksfile) {
+var checkHtml = function(htmlIn, checksFile) {
+    // function does:
+    //  1) uses cheerio to parse the html based on input type - jquery style
+    //  2) uses JSON to load list of tags to check for in html
+    //  3) uses jquery to search html input for each tag in list
+    //
+    // Input:
+    //   - htmlIn : a string containing an html document
+    //   - checksfile : string containing a path to the JSON format list of html tags to check for
+    //
+    // Output:
+    //   An object with each of the tags set as true or 
+    //   false based on presence in the html
+    //
+    $ = cheerioHtmlStr(htmlIn);
     
-    if (DEBUG_LEVEL == 1) { console.error(inStr); }
-    $ = cheerioHtmlStr(inStr);
-    if (DEBUG_LEVEL == 3) { console.error($); }
-    var checks = loadChecks(checksfile).sort();
+    var checks = loadChecks(checksFile).sort();
     var out = {};
+    
     for(var ii in checks) {
-	var present = $(checks[ii]).length > 0;
-	out[checks[ii]] = present;
-    }
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }	
     return out;
 };
 
@@ -127,22 +118,29 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var checkHtmlFile = function(htmlFile,checkFile) {
+    return checkHtml(fs.readFileSync(program.file), program.checks);
+};
+
 if(require.main == module) {
     program
-	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-	.option('-f, --file [html_file]', 'Path to index.html', clone(assertFileExists))
-	.option('-u, --url_to_search [url]', 'URL of html to check', clone(assertUrlForm))
-	.parse(process.argv);
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file [html_file]', 'Path to index.html', clone(assertFileExists))
+        .option('-u, --url [url]', 'URL of html to check', clone(assertUrlForm), URL_DEFAULT)
+        .parse(process.argv);
+    
     if(program.file) {
-	var checkJson = checkHtmlFile(program.file, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
-    } else if(program.url_to_search) { 
-	var checkJson2console = buildfn(program.checks);
-	rest.get(program.url_to_search).on('complete',checkJson2console);
+        console.log("Checking %s for tags listed in %s.\n",[program.file,program.checks]);
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    } else if(program.url) { 
+        var checkJson2Console = buildfn(program.checks);
+        rest.get(program.url).on('complete',checkJson2Console);
     } else {
-	program.help();
+        program.help();
     }
 } else {
+    exports.checkHtml = checkHtml;
     exports.checkHtmlFile = checkHtmlFile;
 }
